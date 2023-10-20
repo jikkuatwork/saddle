23e8b4e81794d1d07ec06a761413a05b791f06a8
Jikku Jose <jikku@storybrain.com>
2023-10-20 18:47:16 +0530

    Save point

---
+++ _components/ResponseBubble.js
@@ -0,0 +1,4 @@
+const ResponseBubble = text => `
+          <div
+            class="p4 text-white text-opacity-90 bg-white bg-opacity-10 tw-text-md px-16 py-4 text-sm mb-4"
+          >${text}</div>`
--- app.js
+++ app.js
@@ -14,47 +14,11 @@ document.addEventListener("alpine:init", () => {
     async handleSend() {
       const input = this.inputField.value
       this.inputField.value = ""
-      const response = await this.replyAsText(input)
+      const response = await ollama.respondInChunks(input)
       this.responseField.innerText = response
-    },
-    async replyAsText(prompt, model = "zephyr") {
-      const url = "http://localhost:11434/api/generate"
-      const payload = {
-        model: model,
-        prompt: prompt,
-      }
-
-      try {
-        const response = await fetch(url, {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify(payload),
-        })
-
-        const data = await response.text()
-        const responses = data
-          .split("\n")
-          .map(r => {
-            try {
-              return JSON.parse(r).response
-            } catch (e) {
-              return null
-            }
-          })
-          .filter(r => r !== null)
-          .join("")
-
-        return responses
-      } catch (error) {
-        console.error("Error:", error)
-        return ""
-      }
     },
   })
 
   window.$S = Alpine.store("app")
+  $S.initialize()
 })
-
-// $S.init()
