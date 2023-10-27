9da3d5b4ffeb04ffa9c6fe292f98b6f4c6dd891a
Jikku Jose <jikku@storybrain.com>
2023-10-27 10:03:40 +0530

    Savepoint

--- app.js
+++ app.js
@@ -4,32 +4,23 @@ document.addEventListener("alpine:init", () => {
     inputField: document.querySelector("#input-field"),
     history: [],
     initialize() {
-      document.addEventListener("keydown", this.logOnEnterPress.bind(this))
+      //
     },
     scrollToBottom() {
       const historyContainer = document.querySelector("#history")
       historyContainer.scrollTop = historyContainer.scrollHeight
     },
-    logOnEnterPress(event) {
-      if (event.key === "Enter") {
-        console.log("enter")
-        this.handleSend()
-      }
-    },
     async handleSend() {
       const input = this.inputField.value
       this.inputField.value = ""
+
       const message = { sender: "user", text: input }
       this.history.push(message)
 
       const response = await ollama.respondInChunks(input)
       const reply = { sender: "system", text: response }
-      this.history.push(reply)
 
-      // this.scrollToBottom()
+      this.history.push(reply)
     },
   })
-
-  window.$S = Alpine.store("app")
-  $S.initialize()
 })
--- saddle.js
+++ saddle.js
@@ -1,10 +1,29 @@
 window.saddle = {
   model: "zephyr:latest",
+  port: "11434",
+  api() {
+    return `http://localhost:${this.port}/api/generate`
+  },
   context: null,
   stream: "",
+  async streamer(prompt, cummilatorCallback, contextCallback = () => null) {
+    this.stream = ""
+    await this.send(prompt).then(r =>
+      this.handleStream(r, fragment => {
+        if (fragment.done) {
+          this.context = fragment.context
+          contextCallback(this.context)
+        } else {
+          this.stream += fragment.response
+          this.stream = DOMPurify.sanitize(marked.parse(this.stream))
+          cummilatorCallback(this.stream)
+        }
+      })
+    )
+  },
   async run(prompt) {
     this.stream = ""
-    await this.postRequest(prompt).then(r =>
+    await this.send(prompt).then(r =>
       this.handleStream(r, f => this.processFragment(f))
     )
 
@@ -19,12 +38,10 @@ window.saddle = {
       return this.stream
     }
   },
-  postRequest(prompt, signal = null) {
+  send(prompt, signal = null) {
     const data = { model: this.model, context: this.context, prompt: prompt }
-    const ollama_host = "localhost"
-    const URL = `http://${ollama_host}:11434/api/generate`
 
-    return fetch(URL, {
+    return fetch(this.api(), {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
