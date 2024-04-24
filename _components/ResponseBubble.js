6111a7d69eae72dbaefef2e8edd0df0e325760a8
Jikku Jose <jikku@storybrain.com>
2024-04-24 10:45:04 +0530

    Match to ollama aesthetics

--- _components/ResponseBubble.js
+++ _components/ResponseBubble.js
@@ -1,6 +1,6 @@
 const ResponseBubble = ({ role, content }) => `
   <div
-    class="p4 markdown-body font-inter text-white text-opacity-90 bg-white bg-opacity-10 px-4 py-4 text-sm tw-max-w-sm rounded ${
+    class="markdown-body font-inter text-opacity-10 bg-dark-200 bg-opacity-10 px-3 py-3 text-sm tw-max-w-sm rounded ${
       role === "user" ? "ml-8" : "mr-8"
     }"
   >${content}</div>
