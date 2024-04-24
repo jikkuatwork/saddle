a32e35c026fa0fef65ca59ef62e4aec2c4b78334
Jikku Jose <jikku@storybrain.com>
2024-04-24 10:59:29 +0530

    Refine UI

--- _components/ResponseBubble.js
+++ _components/ResponseBubble.js
@@ -1,7 +1,9 @@
 const ResponseBubble = ({ role, content }) => `
   <div
-    class="markdown-body font-inter text-opacity-10 bg-dark-200 bg-opacity-10 px-3 py-3 text-sm tw-max-w-sm rounded ${
-      role === "user" ? "ml-8" : "mr-8"
+    class="markdown-body rounded-lg font-inter px-3 py-3 text-sm tw-max-w-sm rounded ${
+      role === "user"
+        ? "ml-8 text-black bg-white border border-gray-100"
+        : "mr-8 bg-dark-100 text-white"
     }"
   >${content}</div>
   `
